9031dd2d046dcce07bc88f0da9a8c2504005a2e5
dhruvathejas <dhruvathejasb@gmail.com>
2025-04-05 10:55:38 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeScoreBy(1)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
+info.onCountdownEnd(function () {
+    game.over(true)
+})
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`TheStage`)
+game.splash("Press \"A\" to play!")
+info.startCountdown(10)
